2f8b68c1409a0c3846f2e96fbbeeae992c9b1c82
HarrisLeow <leow_harris@hotmail.sg>
2016-07-11 12:23:25 +0800

    Deploy for codeship

--- test/revision_tests.js
+++ test/revision_tests.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const supertest = require('supertest');
+const app = require('../app');
 const api = supertest('http://localhost:4000');
 
 describe('GET /', () => {
